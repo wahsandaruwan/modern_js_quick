4c3324c334b4c99d063b8992a58f98b20ec1bdbf
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-02-28 21:09:53 +0530

    Added template literals

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// -----------------------------------
-// ------------Let & Const------------
-// -----------------------------------
+// ----------------------------------------
+// ------------Let & Const(ES6)------------
+// ----------------------------------------
+
+// Var is function scope | let & const are blocked scope
 
 // var num = 20;
 
@@ -28,9 +30,13 @@
 //     let greet = "Hello";
 //     greet = "World!"; // Can't delcare but can replace the value
 //     console.log(greet);
+
+//     let num = 15;
+//     console.log(num);
 // }
 
 // // console.log(greet);
+// console.log(num);
 
 // -----------------------------------
 
@@ -38,8 +44,38 @@
 
 // if(num < 11){
 //     const greet = "Hello";
-//     greet = "World!"; // Can't delcare or replace
+//     // greet = "World!"; // Can't delcare or replace
 //     console.log(greet);
+
+//     const num = 15;
+//     console.log(num);
 // }
 
 // // console.log(greet);
+// console.log(num);
+
+// ----------------------------------------------
+// ------------Template Literals(ES6)------------
+// ----------------------------------------------
+
+// let name = "Kasun";
+// let age = 25;
+
+// let desc = "My name is "+name+" and my age is "+age;
+// console.log(desc);
+
+// -----------------------------------
+
+// let fName = "Chamara";
+// let lName = "Fernando";
+
+// let fullName = `My first name is ${fName} and my last name is ${lName}`;
+// console.log(fullName);
+
+// -----------------------------------
+
+// function myName(name){
+//     return `My name is ${name.toUpperCase()}`;
+// }
+
+// console.log(myName('Nimal Silva'));
