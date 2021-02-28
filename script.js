71d76f089f380c01be4adcc42c47dcfedc3ddbe3
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-02-28 20:44:37 +0530

    Added files and finished let and const

---
+++ script.js
@@ -0,0 +1,45 @@
+// -----------------------------------
+// ------------Let & Const------------
+// -----------------------------------
+
+// var num = 20;
+
+// function multiply(){
+//     var num1  = 30;
+//     console.log(num);
+//     console.log(num1);
+// }
+
+// multiply();
+
+// -----------------------------------
+
+// var a = 40;
+// console.log(a);
+
+// var a = 50;
+// console.log(a);
+
+// -----------------------------------
+
+// let num = 10;
+
+// if(num < 11){
+//     let greet = "Hello";
+//     greet = "World!"; // Can't delcare but can replace the value
+//     console.log(greet);
+// }
+
+// // console.log(greet);
+
+// -----------------------------------
+
+// const num = 10;
+
+// if(num < 11){
+//     const greet = "Hello";
+//     greet = "World!"; // Can't delcare or replace
+//     console.log(greet);
+// }
+
+// // console.log(greet);
